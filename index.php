4ed13590e21a16a4468cf7cc46b6b5efb3a1f500
disappeared <shenruizong@gmail.com>
2013-04-02 12:25:36 +0800

    添加每个页面的默认模板

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ define('APP_PATH',"./Home/");
 define("APP_NAME","Home");
 define('ENGINE_NAME','cluster');
 define("APP_DEBUG",true);
-require( "../ThinkPHP/ThinkPHP.php");
+require( "./ThinkPHP/ThinkPHP.php");
